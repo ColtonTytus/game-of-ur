8f8aceb6e176dbed740e3a0973562a9d28a22436
Alex Kraasch <alex@kraasch.eu>
2026-07-12 09:41:49 +0200

    Add wrapper class for game logic, UrGame.

---
+++ scripts/logic/ur_game.gd
@@ -0,0 +1,70 @@
+extends Node
+class_name  UrGame
+
+#############
+# signals   #
+#############
+
+#signal say_hello
+
+#############
+# enums     #
+#############
+
+#enum MYVALUE {A, B, C}
+
+#############
+# constants #
+#############
+
+#const XXX : String = ""
+
+#############
+# exports   #
+#############
+
+#@export var yyy : String = ""
+
+#############
+# on-readys #
+#############
+
+#@onready var zzz : String = ""
+
+#############
+# varaibles #
+#############
+
+#var xxx : String = ""
+
+#############
+# build-ins #
+#############
+
+#func _ready() -> void:
+	#pass
+
+
+#func _process(delta: float) -> void:
+	#pass
+
+#############
+# listeners #
+#############
+
+#func _on_button_press() -> void:
+#	pass
+
+#############
+# publics   #
+#############
+
+static func can_sb_give_me_a_heeeyaa() -> String:
+	return "Heeyaa"
+
+#############
+# functions #
+#############
+
+#func _read_another_value() -> String:
+#	return XXX
